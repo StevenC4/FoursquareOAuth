6274a7c5856601c9d5830cfa85e8a890e725fc54
Steven Carroll <stevencarroll90@gmail.com>
2016-02-09 19:36:26 -0700

    Reset stylesheet

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="/foursquare/public/stylesheet/main.css">
+	<link rel="stylesheet" type="text/css" href="/foursquare/public/stylesheet/reset.css">
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<script type="text/javascript" src="/foursquare/public/scripts/header.js"></script>
-	<link rel="stylesheet" type="text/css" href="/foursquare/public/stylesheet/main.css">
 	<title>Foursquare OAuth App</title>
 </head>
 <body>
